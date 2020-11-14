857f7e910a1674b3a4b25f01a517a80510aebaaf
LatvianModder <latvianmodder@gmail.com>
2020-11-14 13:22:15 +0200

    First commit

---
+++ src/main/java/com/feed_the_beast/mods/ftbjanitor/FTBJanitor.java
@@ -0,0 +1,28 @@
+package com.feed_the_beast.mods.ftbjanitor;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Locale;
+
+/**
+ * @author LatvianModder
+ */
+@Mod(FTBJanitor.MOD_ID)
+public class FTBJanitor
+{
+	public static final String MOD_ID = "ftbjanitor";
+	public static final String MOD_NAME = "FTB Janitor";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+
+	public FTBJanitor()
+	{
+		Locale.setDefault(Locale.US);
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
+}
