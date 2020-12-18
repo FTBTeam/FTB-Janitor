d7ef862586ff2d1f418beecab578445ab458529d
LatvianModder <latvianmodder@gmail.com>
2020-12-18 19:15:56 +0200

    Changed to cache only default states and fluids

--- src/main/java/com/feed_the_beast/mods/ftbjanitor/core/mixin/AbstractBlockStateMixin.java
+++ src/main/java/com/feed_the_beast/mods/ftbjanitor/core/mixin/AbstractBlockStateMixin.java
@@ -2,6 +2,8 @@ package com.feed_the_beast.mods.ftbjanitor.core.mixin;
 
 import com.feed_the_beast.mods.ftbjanitor.FTBJanitorConfig;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.FlowingFluidBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +18,13 @@ public class AbstractBlockStateMixin
 	@Inject(method = "cacheState", at = @At("HEAD"), cancellable = true)
 	public void cacheState(CallbackInfo ci)
 	{
-		if (FTBJanitorConfig.disableBlockStateCache)
+		AbstractBlock.AbstractBlockState state = (AbstractBlock.AbstractBlockState) (Object) this;
+
+		if (FTBJanitorConfig.disableBlockStateCache
+				&& !(state.getBlock() instanceof AirBlock)
+				&& !(state.getBlock() instanceof FlowingFluidBlock)
+				&& !state.equals(state.getBlock().getDefaultState())
+		)
 		{
 			ci.cancel();
 		}
