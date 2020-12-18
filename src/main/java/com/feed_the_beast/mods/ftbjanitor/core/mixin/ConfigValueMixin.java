40aa4148f5f4c83bb136026fa3582dad1ce5c206
LatvianModder <latvianmodder@gmail.com>
2020-12-18 18:27:05 +0200

    Update config cache if set() is called

--- src/main/java/com/feed_the_beast/mods/ftbjanitor/core/mixin/ConfigValueMixin.java
+++ src/main/java/com/feed_the_beast/mods/ftbjanitor/core/mixin/ConfigValueMixin.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.mutable.MutableLong;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -46,5 +47,11 @@ public abstract class ConfigValueMixin<T>
 			ForgeConfigSpec.ConfigValue<T> configValue = (ForgeConfigSpec.ConfigValue<T>) (Object) this;
 			FTBJanitorCommands.LOGGED_TOML_CONFIGS.computeIfAbsent(String.join(".", configValue.getPath()) + ": " + ci.getReturnValue() + " (from " + new Exception().getStackTrace()[2] + ")", k -> new MutableLong(0L)).increment();
 		}
+	}
+
+	@Inject(method = "set", at = @At(value = "RETURN"), remap = false)
+	private void setFTBJ(T value, CallbackInfo ci)
+	{
+		cachedGetFTBJ = null;
 	}
 }
